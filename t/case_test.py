97c322181210bb6c9109072fd5a3b1ac0af12ea4
Sean McAfee <eefacm@gmail.com>
2007-07-20 07:38:37 +0000

    Translation of Perl test case.t.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@20 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

---
+++ t/case_test.py
@@ -0,0 +1,62 @@
+import template
+from template import test
+
+class CaseTest(test.TestCase):
+  def testCase(self):
+    ttdef = template.Template({ 'POST_CHOMP': 1 })
+    ttanycase = template.Template({ 'ANYCASE': 1, 'POST_CHOMP': 1 })
+    tts = (('default', ttdef), ('anycase', ttanycase))
+    self.Expect(DATA, tts, self._callsign())
+
+DATA = r"""
+-- test --
+[% include = a %]
+[% for = b %]
+i([% include %])
+f([% for %])
+-- expect --
+i(alpha)
+f(bravo)
+
+-- test --
+[% IF a AND b %]
+good
+[% ELSE %]
+bad
+[% END %]
+-- expect --
+good
+
+-- test --
+# 'and', 'or' and 'not' can ALWAYS be expressed in lower case, regardless
+# of CASE sensitivity option.
+[% IF a and b %]
+good
+[% ELSE %]
+bad
+[% END %]
+-- expect --
+good
+
+-- test --
+[% include = a %]
+[% include %]
+-- expect --
+alpha
+
+-- test --
+-- use anycase --
+[% include foo bar='baz' %]
+[% BLOCK foo %]this is foo, bar = [% bar %][% END %]
+-- expect --
+this is foo, bar = baz
+
+-- test --
+[% 10 div 3 %] [% 10 DIV 3 +%]
+[% 10 mod 3 %] [% 10 MOD 3 %]
+-- expect --
+3 3
+1 1
+"""
+
+test.main()
