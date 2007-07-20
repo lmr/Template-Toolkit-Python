9440dbb8ff094e8f909ac0cb650eea28fae4e2ae
Sean McAfee <eefacm@gmail.com>
2007-07-20 20:26:03 +0000

    Translation of t/directive.t.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@26 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

---
+++ t/directive_test.py
@@ -0,0 +1,361 @@
+import template
+from template import test
+
+class DirectiveTest(test.TestCase):
+  def testDirectives(self):
+    ttobjs = (('tt', template.Template()),
+              ('pre', template.Template({ 'PRE_CHOMP': 1 })),
+              ('post', template.Template({ 'POST_CHOMP': 1 })),
+              ('trim', template.Template({ 'INCLUDE_PATH': 'test/lib',
+                                           'TRIM': 1 })))
+    self.Expect(DATA, ttobjs, self._callsign())
+
+
+DATA = """
+#------------------------------------------------------------------------
+# basic directives
+#------------------------------------------------------------------------
+-- test --
+[% a %]
+[%a%]
+-- expect --
+alpha
+alpha
+
+-- test --
+pre [% a %]
+pre[% a %]
+-- expect --
+pre alpha
+prealpha
+
+-- test --
+[% a %] post
+[% a %]post
+-- expect --
+alpha post
+alphapost
+
+-- test --
+pre [% a %] post
+pre[% a %]post
+-- expect --
+pre alpha post
+prealphapost
+
+-- test --
+[% a %][%b%][% c %]
+-- expect --
+alphabravocharlie
+
+-- test --
+[% 
+a %][%b
+%][%
+c
+%][%
+         d
+%]
+-- expect --
+alphabravocharliedelta
+
+#------------------------------------------------------------------------
+# comments
+#------------------------------------------------------------------------
+-- test --
+[%# this is a comment which should
+    be ignored in totality
+%]hello world
+-- expect --
+hello world
+
+-- test -- 
+[% # this is a one-line comment
+   a
+%]
+-- expect --
+alpha
+
+-- test -- 
+[% # this is a two-line comment
+   a =
+   # here's the next line
+   b
+-%]
+[% a %]
+-- expect --
+bravo
+
+-- test --
+[% a = c   # this is a comment on the end of the line
+   b = d   # so is this
+-%]
+a: [% a %]
+b: [% b %]
+-- expect --
+a: charlie
+b: delta
+
+#------------------------------------------------------------------------
+# manual chomping
+#------------------------------------------------------------------------
+
+-- test --
+[% a %]
+[% b %]
+-- expect --
+alpha
+bravo
+
+-- test --
+[% a -%]
+[% b %]
+-- expect --
+alphabravo
+
+-- test --
+[% a -%]
+     [% b %]
+-- expect --
+alpha     bravo
+
+-- test --
+[% a %]
+[%- b %]
+-- expect --
+alphabravo
+
+-- test --
+[% a %]
+     [%- b %]
+-- expect --
+alphabravo
+
+-- test --
+start
+[% a %]
+[% b %]
+end
+-- expect --
+start
+alpha
+bravo
+end
+
+-- test --
+start
+[%- a %]
+[% b -%]
+end
+-- expect --
+startalpha
+bravoend
+
+-- test --
+start
+[%- a -%]
+[% b -%]
+end
+-- expect --
+startalphabravoend
+
+-- test --
+start
+[%- a %]
+[%- b -%]
+end
+-- expect --
+startalphabravoend
+
+#------------------------------------------------------------------------
+# PRE_CHOMP enabled 
+#------------------------------------------------------------------------
+
+-- test --
+-- use pre --
+start
+[% a %]
+mid
+[% b %]
+end
+-- expect --
+startalpha
+midbravo
+end
+
+-- test --
+start
+     [% a %]
+mid
+	[% b %]
+end
+-- expect --
+startalpha
+midbravo
+end
+
+-- test --
+start
+[%+ a %]
+mid
+[% b %]
+end
+-- expect --
+start
+alpha
+midbravo
+end
+
+-- test --
+start
+   [%+ a %]
+mid
+[% b %]
+end
+-- expect --
+start
+   alpha
+midbravo
+end
+
+-- test --
+start
+   [%- a %]
+mid
+   [%- b %]
+end
+-- expect --
+startalpha
+midbravo
+end
+
+#------------------------------------------------------------------------
+# POST_CHOMP enabled 
+#------------------------------------------------------------------------
+
+-- test --
+-- use post --
+start
+[% a %]
+mid
+[% b %]
+end
+-- expect --
+start
+alphamid
+bravoend
+
+-- test --
+start
+     [% a %]
+mid
+	[% b %]        
+end
+-- expect --
+start
+     alphamid
+	bravoend
+
+-- test --
+start
+[% a +%]
+mid
+[% b %]
+end
+-- expect --
+start
+alpha
+mid
+bravoend
+
+-- test --
+start
+[% a +%]   
+[% b +%]
+end
+-- expect --
+start
+alpha   
+bravo
+end
+
+-- test --
+start
+[% a -%]
+mid
+[% b -%]
+end
+-- expect --
+start
+alphamid
+bravoend
+
+
+#------------------------------------------------------------------------
+# TRIM enabled
+#------------------------------------------------------------------------
+-- test --
+-- use trim --
+
+[% INCLUDE trimme %]
+
+
+-- expect --
+I am a template element file which will get TRIMmed
+
+
+-- test --
+[% BLOCK foo %]
+
+this is block foo
+
+[% END -%]
+
+[% BLOCK bar %]
+
+this is block bar
+
+[% END %]
+
+[% INCLUDE foo %]
+[% INCLUDE bar %]
+end
+-- expect --
+this is block foo
+this is block bar
+end
+
+
+-- test --
+<foo>[% PROCESS foo %]</foo>
+<bar>[% PROCESS bar %]</bar>
+[% BLOCK foo %]
+
+this is block foo
+
+[% END -%]
+[% BLOCK bar %]
+
+this is block bar
+
+[% END -%]
+end
+-- expect --
+<foo>this is block foo</foo>
+<bar>this is block bar</bar>
+end
+
+
+-- test --
+[% r; r = s; "-"; r %].
+-- expect --
+romeo-sierra.
+
+-- test --
+[% IF a; b; ELSIF c; d; ELSE; s; END %]
+-- expect --
+bravo
+
+"""
+
+test.main()
