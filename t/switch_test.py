790579de2f1124a19c18650806f0d1633b9be5d4
Sean McAfee <eefacm@gmail.com>
2007-07-25 07:45:50 +0000

    Translation of t/switch.t.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@46 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

---
+++ t/switch_test.py
@@ -0,0 +1,261 @@
+from template import test
+
+
+class SwitchTest(test.TestCase):
+  def testSwitch(self):
+    ttcfg = { 'POST_CHOMP': True }
+    self.Expect(DATA, ttcfg, self._callsign())
+
+
+DATA = r"""
+#------------------------------------------------------------------------
+# test simple case
+#------------------------------------------------------------------------
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% END %]
+after
+
+-- expect --
+before
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE x %]
+not matched
+[% END %]
+after
+
+-- expect --
+before
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE not_defined %]
+not matched
+[% END %]
+after
+
+-- expect --
+before
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE 'alpha' %]
+matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE a %]
+matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH 'alpha' %]
+this is ignored
+[% CASE a %]
+matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE b %]
+matched
+[% END %]
+after
+
+-- expect --
+before
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE a %]
+matched
+[% CASE b %]
+not matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE b %]
+not matched
+[% CASE a %]
+matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+#------------------------------------------------------------------------
+# test default case
+#------------------------------------------------------------------------
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE a %]
+matched
+[% CASE b %]
+not matched
+[% CASE %]
+default not matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE a %]
+matched
+[% CASE b %]
+not matched
+[% CASE DEFAULT %]
+default not matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE z %]
+not matched
+[% CASE x %]
+not matched
+[% CASE %]
+default matched
+[% END %]
+after
+
+-- expect --
+before
+default matched
+after
+
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE z %]
+not matched
+[% CASE x %]
+not matched
+[% CASE DEFAULT %]
+default matched
+[% END %]
+after
+
+-- expect --
+before
+default matched
+after
+
+#------------------------------------------------------------------------
+# test multiple matches
+#------------------------------------------------------------------------
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE [ a b c ] %]
+matched
+[% CASE d %]
+not matched
+[% CASE %]
+default not matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+-- test --
+before
+[% SWITCH a %]
+this is ignored
+[% CASE [ a b c ] %]
+matched
+[% CASE a %]
+not matched, no drop-through
+[% CASE DEFAULT %]
+default not matched
+[% END %]
+after
+
+-- expect --
+before
+matched
+after
+
+"""
+
+test.main()
+
+
+
+
+
