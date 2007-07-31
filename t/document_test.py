20947a43d237a02a6af0fb29c7ce5189ff405292
Sean McAfee <eefacm@gmail.com>
2007-07-31 10:03:34 +0000

    Translation of Perl's t/document.t.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@74 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

---
+++ t/document_test.py
@@ -0,0 +1,119 @@
+import template
+from template import document, test
+
+
+#  Define a dummy object for runtime processing
+class DummyContext:
+  def visit(self, *args):
+    pass
+  def leave(self, *args):
+    pass
+
+
+class DocumentTest(test.TestCase):
+  def testDocument(self):
+    # Create a document and check accessor methods for blocks and metadata
+    doc = document.Document(
+      { 'BLOCK': lambda *_: 'some output',
+        'DEFBLOCKS': { 'foo': lambda *_: 'the foo block',
+                       'bar': lambda *_: 'the bar block' },
+        'METADATA': { 'author': 'Andy Wardley',
+                      'version': 3.14 }})
+    c = DummyContext()
+    self.failUnless(doc)
+    self.assertEquals('Andy Wardley', doc.author)
+    self.assertEquals(3.14, doc.version)
+    self.assertEquals('some output', doc.process(c))
+    self.failUnless(callable(doc.block()))
+    self.failUnless(callable(doc.blocks()['foo']))
+    self.failUnless(callable(doc.blocks()['bar']))
+    self.assertEquals('some output', doc.block()())
+    self.assertEquals('the foo block', doc.blocks()['foo']())
+    self.assertEquals('the bar block', doc.blocks()['bar']())
+    
+    tproc = template.Template({ 'INCLUDE_PATH': 'test/src' })
+    self.Expect(DATA, tproc, { 'mydoc': doc })
+
+DATA = r"""
+-- test --
+# test metadata
+[% META
+   author = 'Tom Smith'
+   version = 1.23 
+-%]
+version [% template.version %] by [% template.author %]
+-- expect --
+version 1.23 by Tom Smith
+
+# test local block definitions are accessible
+-- test --
+[% BLOCK foo -%]
+   This is block foo
+[% INCLUDE bar -%]
+   This is the end of block foo
+[% END -%]
+[% BLOCK bar -%]
+   This is block bar
+[% END -%]
+[% PROCESS foo %]
+
+-- expect --
+   This is block foo
+   This is block bar
+   This is the end of block foo
+
+-- test --
+[% META title = 'My Template Title' -%]
+[% BLOCK header -%]
+title: [% template.title or title %]
+[% END -%]
+[% INCLUDE header %]
+-- expect --
+title: My Template Title
+
+-- test --
+[% BLOCK header -%]
+HEADER
+component title: [% component.name %]
+ template title: [% template.name %]
+[% END -%]
+component title: [% component.name %]
+ template title: [% template.name %]
+[% PROCESS header %]
+-- expect --
+component title: input text
+ template title: input text
+HEADER
+component title: header
+ template title: input text
+
+-- test --
+[% META title = 'My Template Title' -%]
+[% BLOCK header -%]
+title: [% title or template.title  %]
+[% END -%]
+[% INCLUDE header title = 'A New Title' %]
+[% INCLUDE header %]
+-- expect --
+title: A New Title
+
+title: My Template Title
+
+-- test --
+[% INCLUDE $mydoc %]
+-- expect --
+some output
+
+-- stop --
+# test for component.caller and component.callers patch
+-- test --
+[% INCLUDE one;
+   INCLUDE two;
+   INCLUDE three;
+%]
+-- expect --
+one, three
+two, three
+"""
+
+test.main()
