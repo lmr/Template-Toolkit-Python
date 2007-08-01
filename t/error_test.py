704675fba6190e382c1e8836dd24af2dfc3631f6
Sean McAfee <eefacm@gmail.com>
2007-08-01 06:14:59 +0000

    Translation of Perl's t/error.t.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@75 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

---
+++ t/error_test.py
@@ -0,0 +1,17 @@
+import template
+from template import base, test
+
+
+class ErrorTest(test.TestCase):
+  def testError(self):
+    tmpl = template.Template(
+      { 'BLOCKS': { 'badinc': '[% INCLUDE nosuchfile %]' } })
+    self.assert_(not tmpl.process('badinc'))
+    error = tmpl.error()
+    self.assert_(error)
+    self.assert_(isinstance(error, base.Exception))
+    self.assertEquals('file', error.type)
+    self.assertEquals('nosuchfile: not found', error.info)
+
+
+test.main()
