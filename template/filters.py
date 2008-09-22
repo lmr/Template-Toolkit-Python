5d0db774199d5beca23fc076a9496e6564b3ef95
Sean McAfee <eefacm@gmail.com>
2008-09-22 02:21:14 +0000

    Adds a copyright notice and a reference to the LICENSE file to the top of every source file.
    git-svn-id: svn://svn.tt2.org/Template-Python/trunk@168 7f301bf4-a02f-0410-90b8-a3b0f3303b9a

--- template/filters.py
+++ template/filters.py
@@ -1,4 +1,13 @@
 # coding: latin-1
+#
+#  The Template-Python distribution is Copyright (C) Sean McAfee 2007-2008,
+#  derived from the Perl Template Toolkit Copyright (C) 1996-2007 Andy
+#  Wardley.  All Rights Reserved.
+#
+#  The file "LICENSE" at the top level of this source distribution describes
+#  the terms under which this file may be distributed.
+#
+
 import errno
 import os
 import re
