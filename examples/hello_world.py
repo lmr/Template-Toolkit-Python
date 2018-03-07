3b42a9d1d39cddcc2f1ed92d202e1cfce2c968ce
Lucas Meneghel Rodrigues <lookkas@gmail.com>
2018-03-07 18:45:27 +0100

    More PEP8 Fixes
    Signed-off-by: Lucas Meneghel Rodrigues <lookkas@gmail.com>

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -2,8 +2,7 @@ from template import Template
 from template.util import Literal
 
 template = Template()
-source   = Literal("Hello [% name or 'World' %]!")
+source = Literal("Hello [% name or 'World' %]!")
 
-print(template.process(source));
-print(template.process(source, {'name':'Badger'}));
-
+print(template.process(source))
+print(template.process(source, {'name': 'Badger'}))
--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@
 
 from distutils.core import setup
 
-setup(name         = 'Template-Python',
-      version      = '0.1.post1',
-      description  = 'Python port of the Template Toolkit',
-      author       = 'Sean McAfee',
-      author_email = 'eefacm@gmail.com',
-      url          = 'http://template-toolkit.org/python/',
-      packages     = ['template', 'template.plugin', 'template.namespace'],
-      package_dir  = { 'template.plugin': 'template/plugin', 'template.namespace': 'template/namespace' },
-     )
+setup(name='Template-Python',
+      version='0.1.post1',
+      description='Python port of the Template Toolkit',
+      author='Sean McAfee',
+      author_email='eefacm@gmail.com',
+      url='http://template-toolkit.org/python/',
+      packages=['template', 'template.plugin', 'template.namespace'],
+      package_dir={'template.plugin': 'template/plugin', 'template.namespace': 'template/namespace'},
+      )
